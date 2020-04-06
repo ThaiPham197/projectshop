2ef198768b2ddf5a96f246750eea831c32ebbf54
thaiph197 <zangaga197@gmail.com>
2020-04-06 22:09:33 +0700

    add domain entity and add EF code first for data

---
+++ Shop.Model/Abstract/Auditable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { get; set; }
+
+        [MaxLength(256)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        [MaxLength(256)]
+        public string UpdateBy { get; set; }
+    }
+}
---
+++ Shop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdateBy { get; set; }
+    }
+}
